9475901cabb6cd110754c4b96655eb418d73026f
elly <driellymaciel@hotmail.com>
2021-11-06 13:18:38 -0300

    resolvendo erro de exportação de módulo

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,8 +6,8 @@ module br.ufrpe.sosb {
     requires com.dlsc.formsfx;
     requires org.kordamp.bootstrapfx.core;
 
-    exports br.ufrpe.sos.beans;
-    opens br.ufrpe.sos.beans to javafx.fxml;
+    exports br.ufrpe.sos.beans.animal;
+    opens br.ufrpe.sos.beans.animal to javafx.fxml;
     exports br.ufrpe.sos.data;
     opens br.ufrpe.sos.data to javafx.fxml;
     exports br.ufrpe.sos.beans.pessoa;
