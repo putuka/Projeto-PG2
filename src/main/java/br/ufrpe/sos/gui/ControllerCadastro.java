72ffc0cf44f187f7ab8c092ac0dd34ede3ae24bc
elly <driellymaciel@hotmail.com>
2021-11-26 02:40:46 -0300

    impl do método para validar os campos do formulário e exibir mensagem dos campos errados e caso o cadastro for concluido

--- src/main/java/br/ufrpe/sos/gui/ControllerCadastro.java
+++ src/main/java/br/ufrpe/sos/gui/ControllerCadastro.java
@@ -5,6 +5,7 @@ import br.ufrpe.sos.beans.pessoa.Pessoa;
 import br.ufrpe.sos.controller.Facades;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 
@@ -30,27 +31,34 @@ public class ControllerCadastro {
     private TextField txtCep;
 
     @FXML
-    private void initialize(){
-
-    }
-
-    @FXML
     protected void login(ActionEvent e ){
         Main.trocaDeTela("telaLogin");
     }
+    @FXML
+    protected void voltar(ActionEvent e) {Main.trocaDeTela("telaMenu");}
+
+    public void voltarAoMenu(ActionEvent event){
+        this.limparCampos();
+        this.voltar(event);
+    }
 
     public void cadastrarPessoa(ActionEvent event){
-        Endereco e = new Endereco(this.txtRua.getText(), this.txtCidade.getText(),this.txtEstado.getText(), this.txtCep.getText());
-        Pessoa p = new Pessoa(this.txtNome.getText(),
-                this.txtTelefone.getText(), this.txtCpf.getText(), this.txtEmail.getText(), this.dataNascimento.getValue(), e);
-        try {
-            Facades.getInstance().inserir(p);
-        }catch (Exception exception){
-            // TODO Tratar exceção com mensagem na tela
+        if (validarInputs()) {
+            Endereco e = new Endereco(this.txtRua.getText(), this.txtCidade.getText(), this.txtEstado.getText(), this.txtCep.getText());
+            Pessoa p = new Pessoa(this.txtNome.getText(),
+                    this.txtTelefone.getText(), this.txtCpf.getText(), this.txtEmail.getText(), this.dataNascimento.getValue(), e);
+            try {
+                Facades.getInstance().inserir(p);
+            } catch (Exception exception) {
+                // TODO Tratar exceção com mensagem na tela
+            }
+            this.limparCampos();
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Cadastro Efetuado");
+            alert.setHeaderText("Cadastrado efetuado com Sucesso!");
+            alert.showAndWait();
+            this.login(event);
         }
-        this.limparCampos();
-        this.login(event);
-        //((Stage) ((Node) event.getSource()).getScene().getWindow()).close();
     }
 
     public void limparCampos(){
@@ -65,6 +73,60 @@ public class ControllerCadastro {
         this.txtCep.setText("");
     }
 
-    //TODO Validação dos campos
+    private boolean validarInputs(){
+        String errorMessage = "";
+
+        if (txtNome.getText() == null || txtNome.getText().length() == 0){
+            errorMessage += "Preencha o nome!\n";
+        }
+        if (txtTelefone.getText() == null || txtTelefone.getText().length() == 0){
+            errorMessage += "Preencha o telefone!\n";
+        }else {
+            try {
+                Integer.parseInt(txtTelefone.getText());
+            }catch (NumberFormatException e){
+                errorMessage += "Telefone inválido\n";
+            }
+        }
+        if (txtCpf.getText() == null || txtCpf.getText().length() == 0){
+            errorMessage += "Preencha o cpf!\n";
+        }
+        if (txtEmail.getText() == null || txtEmail.getText().length() == 0){
+            errorMessage += "Preencha o email!\n";
+        }
+        if (dataNascimento.getValue() == null){
+            errorMessage += "Informe sua data de nascimento!\n";
+        }
+        if (txtRua.getText() == null || txtRua.getText().length() == 0){
+            errorMessage += "Preencha a rua!\n";
+        }
+        if (txtCidade.getText() == null || txtCidade.getText().length() == 0){
+            errorMessage += "Preencha a cidade!\n";
+        }
+        if (txtEstado.getText() == null || txtEstado.getText().length() == 0){
+            errorMessage += "Preencha o estado!\n";
+        }
+        if (txtCep.getText() == null || txtCep.getText().length() == 0){
+            errorMessage += "Preencha o CEP!\n";
+        }else {
+            try {
+                Integer.parseInt(txtCep.getText());
+            }catch (NumberFormatException e){
+                errorMessage += "CEP inválido\n";
+            }
+        }
+
+        if (errorMessage.length() == 0){
+            return true;
+        }else {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Campos do cadastro inválido");
+                alert.setHeaderText("Por favor, corrija os campos inválidos para que possamos concluir seu cadastro!");
+                alert.setContentText(errorMessage);
+            alert.showAndWait();
+
+            return false;
+        }
+    }
 
 }
